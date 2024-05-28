8a73e9a3f29e085f0af82cfa79244a3f6f839690
JulioFrancia <julio90lkr@gmail.com>
2024-05-28 00:34:29 -0500

    se agrego el llamado del header y footer con include

---
+++ Pasteleria/encabezado.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crumble&Flake</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Si tienes un archivo de estilos CSS -->
+</head>
+<body>
+    <nav class="main-header">
+        <container>
+            <div id="logo-header">
+                <span class="site-name">Crumble&Flake.com</span>
+                <span class="site-desc">Pasteles / Postres / Bocaditos</span>
+            </div>
+
+            <div class="nav_menu">
+                <ul>
+                    <li><a href="index.PHP"><img src="https://getconnected.honeywellhome.com/en/sites/getconnected.honeywell.com.en/files/lyric_to_home.png" height="18" width="18" style="vertical-align: middle;"> Home</a></li>
+                    <li><a href="Comprar.PHP"><img src="https://icones.pro/wp-content/uploads/2021/05/icone-de-panier-noir.png" width="20" height="20" style="vertical-align: middle;"> Comprar</a></li>
+                    <li><a href="Carrito.PHP"><img src="Imagenes/carrito.png" width="40" height="40" style="vertical-align: middle;"> Carrito</a></li>
+                    <li><a href="eleccion_sesion.PHP"><button class="nav--button" href="#">Iniciar sesión</button></a></li>
+                </ul>
+            </div>
+        </container>
+    </nav>
+</body>
+</html>
+
